361b81b48f314aa2234c96e85b7aac755050ee93
가현 <bufar1014@gmail.com>
2025-12-12 02:41:45 +0900

    영화 객체 생성자 추가

--- lib/model/model_movie.dart
+++ lib/model/model_movie.dart
@@ -3,4 +3,17 @@ class Movie {
   late final String title;
   late final String keyword;
   late final String poster;
+  late bool like;
+
+  // Map을 받아서 Movie 객체를 생성하는 생성자
+  // JSON/Map 데이터를 Movie 객체로 변환하는 factory-like 생성자
+  Movie.fromMap(Map<String, dynamic> map)
+  : title = map['title'],
+    keyword =map['keyword'],
+    poster = map['poster'],
+    like = map['like'];
+
+  // 인스턴스를 출력할 때 용이하게 하기 위해
+  @override
+  String toString() => "Movie<$title:$keyword>";
 }
