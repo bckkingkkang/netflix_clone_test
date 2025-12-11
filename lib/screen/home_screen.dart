2b86a599274ef7d448975b8e436c62f673e33d75
가현 <bufar1014@gmail.com>
2025-12-12 02:25:53 +0900

    home_screen.dart 에서 TopBar 설정
    해당 TopBar가 잘 표시되는지 확인하기 위해 임시로 return 값 변경

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -16,10 +16,25 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    return TopBar();
+  }
+}
+
+// Home 화면의 상단바 같은 경우 home 화면에서만 사용되기 때문에 별도의 위젯으로 분리하지 않음
+class TopBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child : Center(
-        child : Text('Home Screen'),
-      ),
+      padding : EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child : Row(
+        children : <Widget> [
+          Image.asset(
+            'images/Netflix_test.png', 
+            fit: BoxFit.contain, 
+            height: 25,
+          ),
+        ]
+      )
     );
   }
 }
