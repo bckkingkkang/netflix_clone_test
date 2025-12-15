34f30e6a91415ab9e1dcf8cb04e474b9c9bb7298
가현 <bufar1014@gmail.com>
2025-12-15 23:59:44 +0900

    원형 영화 위젯 생성

---
+++ lib/widget/circle_slider.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone_test/model/model_movie.dart';
+
+// 상태변화가 없기 때문에 StatelessWidget 사용
+class CircleSlider extends StatelessWidget {
+
+  // 기본 구조 작성 후 home_screen.dart에서 movies를 넘겨받아야 한다.
+  late final List<Movie> movies;
+  // 생성자 초기화
+  CircleSlider({required this.movies});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding : EdgeInsets.all(7),
+      child : Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('미리보기'),
+          Container(
+            height: 120,
+            // 리스트 뷰 작성
+            child : ListView(
+              scrollDirection: Axis.horizontal, // 가로 방향 스크롤
+              // 실제적인 위젯들을 만드는 부분 - 별도의 함수로 분리
+              children : makeCircleImages(movies),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
+
+List<Widget> makeCircleImages(List<Movie> movies) {
+  List<Widget> results = [];
+
+  for(var i = 0; i<movies.length; i++) {
+    results.add(
+      InkWell(
+        onTap : () {},
+        child : Container(
+          padding : EdgeInsets.only(right:30),
+          child : Align(
+            alignment:Alignment.centerLeft,
+            child : CircleAvatar(
+              backgroundImage : AssetImage(movies[i].poster),
+              radius : 48,
+            )
+          )
+        )
+      )
+    );
+  }
+  return results;
+}
