7af450483347104f4106003a1d3aa809bc3ed29c
가현 <bufar1014@gmail.com>
2025-12-16 00:05:11 +0900

    BoxSlider 위젯 home_screen에 표시

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_test/model/model_movie.dart';
+import 'package:netflix_clone_test/widget/box_slider.dart';
 import 'package:netflix_clone_test/widget/carousel_slider.dart';
 import 'package:netflix_clone_test/widget/circle_slider.dart';
 
@@ -69,7 +70,8 @@ class _HomeScreenState extends State<HomeScreen> {
           TopBar(),
         ]
       ),
-      CircleSlider(movies: movies)
+      CircleSlider(movies: movies),
+      BoxSlider(movies: movies,)
     ]);
   }
 }
