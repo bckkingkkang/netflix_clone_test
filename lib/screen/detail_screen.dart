c29365a9b169e03226d58e17268930d49a7a72cf
가현 <bufar1014@gmail.com>
2025-12-17 05:38:53 +0900

    상세 화면(detail_screen)기본 구조 세팅

---
+++ lib/screen/detail_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatefulWidget{
+  @override
+  _DetailScreenState createState() => _DetailScreenState();
+
+}
+
+class _DetailScreenState extends State<DetailScreen>{
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+
+    );
+  }
+}
