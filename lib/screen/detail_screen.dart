14ec1f15929ccef427a2c8c7564d9fcd051c0ba6
가현 <bufar1014@gmail.com>
2025-12-17 05:41:32 +0900

    Movie 객체 생성 후 찜하기 상태 가져오기

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone_test/model/model_movie.dart';
 
 class DetailScreen extends StatefulWidget{
+  final Movie movie;
+  DetailScreen({required this.movie});
+
   @override
   _DetailScreenState createState() => _DetailScreenState();
 
 }
 
 class _DetailScreenState extends State<DetailScreen>{
+  bool like = false;
+
   @override
   void initState() {
     super.initState();
+    like = widget.movie.like;
   }
 
   @override
