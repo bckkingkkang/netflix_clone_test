1cda7084014220a6a8e857dfcf04b1b6700a8c0c
가현 <bufar1014@gmail.com>
2025-12-18 04:34:23 +0900

    메인 화면에 반영

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_test/screen/home_screen.dart';
+import 'package:netflix_clone_test/screen/more_screen.dart';
 import 'package:netflix_clone_test/widget/bottom_bar.dart';
 
 void main() => runApp(MyApp());
@@ -42,10 +43,7 @@ class _MyAppState extends State<MyApp> {
                   child : Text('Save'),
                 )
               ),
-              Container(child : Center(
-                  child : Text('More'),
-                )
-              ),
+              MoreScreen(),
             ]
           ),
           // Navigation Bar 설정
