7704e6558f11ba30155daa52bf29ca7d312f9baa
가현 <bufar1014@gmail.com>
2025-12-20 22:29:09 +0900

    메인화면에 searchScreen 표시되도록 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_test/screen/home_screen.dart';
 import 'package:netflix_clone_test/screen/more_screen.dart';
+import 'package:netflix_clone_test/screen/serach_screen.dart';
 import 'package:netflix_clone_test/widget/bottom_bar.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -40,10 +41,7 @@ class _MyAppState extends State<MyApp> {
             // children 설정 - 각 탭에 해당하는 위젯들
             children : <Widget> [
               HomeScreen(),
-              Container(child : Center(
-                  child : Text('Search'),
-                )
-              ),
+              SearchScreen(),
               Container(child : Center(
                   child : Text('Save'),
                 )
