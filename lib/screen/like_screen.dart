8372708527b9e912281b5c03e7c04e9bcadd7484
가현 <bufar1014@gmail.com>
2025-12-21 20:24:46 +0900

    찜한 콘텐츠를 표시할 like_sceen 파일 생성

---
+++ lib/screen/like_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class likeScreen extends StatefulWidget {
+  @override
+  _likeScreenState createState() => _likeScreenState();
+}
+
+class _likeScreenState extends State<likeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
